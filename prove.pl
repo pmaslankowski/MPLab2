dc1297d818b0140d95cc995914e10df1b2b8ac70
pma <pmaslankowski@gmail.com>
2016-04-01 00:11:57 +0200

    Faster version - works.

--- prove.pl
+++ prove.pl
@@ -30,23 +30,44 @@ order(>, L1, L2) :-
 	length(L2, Y),
 	X >= Y.
 
+init([], MapAcc, _, [], MapAcc, []).
+init([H|T], MapAcc, CurrentNumber, [HR|TR], Map, [(H,axiom)|Proof]) :-
+	normClause(H, HR), !,
+	put_assoc(HR, MapAcc, CurrentNumber, NewMapAcc),
+	NewNumber is CurrentNumber + 1,
+	init(T, NewMapAcc, NewNumber, TR, Map, Proof).
+init(Clauses, NormalisedClauses, Map, Proof) :-
+	empty_assoc(MapAcc),
+	init(Clauses, MapAcc, 1, NormalisedClauses, Map, Proof).
+
 resolve(C1, C2, R) :-
 	select(V, C1, C1V),
 	not(V, NV),
 	select(NV, C2, C2NV),
 	ord_union([C1V, C2NV], R).
-resolve(Clauses, R) :-
+resolve(Clauses, Map, I1, I2, R) :-
 	member(C1, Clauses),
 	member(C2, Clauses),
 	C1 \= C2,
 	resolve(C1, C2, R),
 	\+member(R, Clauses),
-	isNotTrue(R), !.
+	isNotTrue(R), !, 
+	get_assoc(C1, Map, I1),
+	get_assoc(C2, Map, I2).
 
-prove(Clauses) :- 
-	resolve(Clauses, []), !.
-prove(Clauses) :-
-	print(Clauses), nl,
-	resolve(Clauses, R),
+prove(Clauses, _, Map, ProofAcc, Proof) :- 
+	resolve(Clauses, Map, I1, I2, []), !,
+	append(ProofAcc, [([], I1, I2)], Proof).
+prove(Clauses, CurrentNumber, Map, ProofAcc, Proof) :-
+	resolve(Clauses, Map, I1, I2, R),
 	predsort(order, [R|Clauses], NewClauses),
-	prove(NewClauses).
+	put_assoc(R, Map, CurrentNumber, NewMap),
+	NewNumber is CurrentNumber+1,
+	normClause_(Normalised, R),
+	append(ProofAcc, [(Normalised, I1, I2)], NewProofAcc),
+	prove(NewClauses, NewNumber, NewMap, NewProofAcc, Proof).
+prove(Clauses, Proof) :-
+	init(Clauses, NormalisedClauses, Map, StartProof),
+	length(Clauses, CurrNumber),
+	StartNumber is CurrNumber+1,
+	prove(NormalisedClauses, StartNumber, Map, StartProof, Proof).
